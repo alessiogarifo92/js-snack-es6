87e2e66e62aaba56de4ae92fef2ed310f989296a
alessiogarifo92 <alessiogarifo92@gmail.com>
2020-11-13 10:29:30 +0100

    corr in class semplificando filter e agg nuovi commenti uso filter

--- jsnack 1 pomeriggio/js/script.js
+++ jsnack 1 pomeriggio/js/script.js
@@ -16,10 +16,11 @@ console.log(numeroDue);
 
 // usiamo filter di myArray per stabilire condizione che nel caso index myArray >= numero inserito in prompt e index myArray <= valore inserito secondo prompt, allora deve ritornare un nuovoArray con elementi con index compresi nelle condizioni indicate
 const nuovoArray =  myArray.filter((element, index) => {
-  if (index >= numeroUno && index <= numeroDue) {
-    console.log(element , index);
-    return element; //anche scrivendo index riporta comunque non il numero index ma i nomi dell' array
-  }
+  // if (index >= numeroUno && index <= numeroDue) {
+    // console.log(element , index);
+    // return element; //anche scrivendo index riporta comunque non il numero index ma i nomi dell' array
+  // }
+  return (index >= numeroUno && index <= numeroDue) || (index >= numeroDue && index <= numeroUno) //non importava fare if perche in filter bisogna inserire la condizione di ritorno e inserisco anche condizione se numeroDue piu piccolo di numeroUno //filter fa ritornare solo cio che viene soddisfatto nella condizione quindi se return condizione true mi eseguira il filter altrimenti no
 });
 
 console.log(nuovoArray);
